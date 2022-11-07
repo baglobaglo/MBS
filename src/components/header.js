cf33c7e20aa6c1e5d8edc61b2a99f700d42b28bf
baglobaglo <baglosebastian@gmail.com>
2022-11-07 12:24:26 -0600

    First initial cleanups. Added components for header and login

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div id="header-div">
+      <p id="header-mbs-name">MBS - Movie Booking System</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+  return (
+    <div>
+      <h3 id="test-login">Login component</h3>
+    </div>
+  );
+}
+
+export default Login;
