892edce48a84ff30501969733b7f4332fc2a3194
baglobaglo <baglosebastian@gmail.com>
2022-11-07 16:22:31 -0600

    fix:

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import Login from './components/login';
 import Header from './components/header';
-import Signup from './components/sign-up';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Signup />
     <Login />
   </React.StrictMode>
 );
