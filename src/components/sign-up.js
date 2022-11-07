6a867782eb33da2c271ebbeed821d92667b414a2
baglobaglo <baglosebastian@gmail.com>
2022-11-07 16:16:46 -0600

    added sign-up component

---
+++ src/components/sign-up.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-up.css';
+
+function Signup() {
+  return (
+    <div id="sign-up-div">
+      <h3>Sign-up component</h3>
+    </div>
+  );
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import Login from './components/login';
 import Header from './components/header';
+import Signup from './components/sign-up';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Signup />
     <Login />
   </React.StrictMode>
 );
