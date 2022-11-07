608660d42e7ca7c03a1b0b6d098a9b53c5dece56
baglobaglo <baglosebastian@gmail.com>
2022-11-07 17:18:50 -0600

    fix:

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './login.css';
-import Signup from './sign-up';
 
 function Login() {
   return (
     <div>
-      <Signup />
       <h3 id="test-login">Login component hello</h3>
     </div>
   );
