b1725b10fe9ec2eb02b54fc7566b9922b790f17c
Taylor <taylor.dobson@ttu.edu>
2022-11-10 12:38:43 -0600

    td

---
+++ src/components/catalog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './catalog.css';
+import './general.css';
+
+function Catalog() {
+  return (
+    
+    <div id="catalog-div">
+        <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/blackkklansman2-web.jpg" width="400" height="550" alt="KKK"/>
+    </div>
+  );
+}
+
+export default Catalog;
