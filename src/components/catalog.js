921ece6e662286ee61dcd3e535927dc58592b9fd
Taylor <taylor.dobson@ttu.edu>
2022-11-11 10:36:13 -0600

    td

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -5,7 +5,8 @@ import './general.css';
 function Catalog() {
   return (
     <div id="catalog-div">
-        <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1025274024420462652/GhidoraCS.png" width="400" height="550" alt="Poster"/>
+      <h3> CATALOG </h3>
+        <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1025274024420462652/GhidoraCS.png" class="posters" alt="poster"/>
     </div>
   );
 }
