40f2ce9532732c0cb8a88d07c6ff5b70cb442358
Taylor <taylor.dobson@ttu.edu>
2022-11-11 10:28:07 -0600

    td

--- src/components/login.js
+++ src/components/login.js
@@ -5,13 +5,13 @@ import './general.css';
 function Login() {
   return (
     <div id="login-div">
+      <h3> LOGIN </h3>
       <body>
-        <h3>LOGIN</h3>
-          <form action="/login-page">
-            Email Address: <input type="text" /><br/>
-            Password: <input type="text" /><br/>
-            <button type="Login">Login</button>
-          </form>
+        <form action="/login-page">
+          Email Address: <input type="text"/> <br/>
+          Password: <input type="text"/> <br/>
+          <button type="Login"> Login </button>
+        </form>
       </body>
     </div>
   );
--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -5,16 +5,16 @@ import './general.css';
 function Signup() {
   return (
     <div id="sign-up-div">
-      <h3>REGISTER</h3>
+      <h3> REGISTER </h3>
       <body>
         <form action="/sign-up-page">
-          Full Name: <input type="text" /><br/>
-          Email Address: <input type="text" /><br/>
-          Home Address: <input type="text" /><br/>
-          Phone Number: <input type="text" /><br/>
-          Password: <input type="text" /><br/>
-          Confirm Password: <input type="text" /><br/>
-          <button type="Register">Register</button>
+          Full Name: <input type="text"/> <br/>
+          Email Address: <input type="text"/> <br/>
+          Home Address: <input type="text"/> <br/>
+          Phone Number: <input type="text"/> <br/>
+          Password: <input type="text"/> <br/>
+          Confirm Password: <input type="text"/> <br/>
+          <button type="Register"> Register </button>
         </form>
       </body>
     </div>
