9d76d8350feea53ba5ad34e5e47b82fc2cc57b8c
Taylor <taylor.dobson@ttu.edu>
2022-11-11 10:10:59 -0600

    td

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -4,9 +4,8 @@ import './general.css';
 
 function Catalog() {
   return (
-    
     <div id="catalog-div">
-        <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/blackkklansman2-web.jpg" width="400" height="550" alt="KKK"/>
+        <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1025274024420462652/GhidoraCS.png" width="400" height="550" alt="Poster"/>
     </div>
   );
 }
