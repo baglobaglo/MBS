1d7bdf2b228004d9bac8579b4706829360e331d2
Taylor <taylor.dobson@ttu.edu>
2022-11-11 10:39:01 -0600

    td

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -7,6 +7,8 @@ function Catalog() {
     <div id="catalog-div">
       <h3> CATALOG </h3>
         <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1025274024420462652/GhidoraCS.png" class="posters" alt="poster"/>
+        <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1025274025146064916/Dang_Chi.png" class="posters" alt="poster"/>
+        <img src="https://cdn.discordapp.com/attachments/1025272411723145277/1040310343743836241/SWnewhope.png" class="posters" alt="poster"/>
     </div>
   );
 }
