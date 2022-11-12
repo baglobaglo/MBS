5a00c1f77812a477dad1856cad62aee44b0b71e2
baglobaglo <baglosebastian@gmail.com>
2022-11-11 18:40:20 -0600

    God help

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import Login from './components/login';
 import Header from './components/header';
-import Signup from './components/sign-up';
+import Catalog from './components/catalog';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Signup />
     <Login />
+    <Catalog />
   </React.StrictMode>
 );
