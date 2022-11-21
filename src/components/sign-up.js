a435e7ba55c53a588cba1ace839fc933d3f728b6
baglobaglo <baglosebastian@gmail.com>
2022-11-21 16:38:20 -0600

    feature: fixed signup page to check inputdata

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,19 +1,83 @@
 import React from 'react';
 import './sign-up.css';
 import './general.css';
+import { useState } from 'react';
 
 function Signup() {
+  const [registerName, setRegisterName] = useState('');
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerHome, setRegisterHome] = useState('');
+  const [registerPhone, setRegisterPhone] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [registerPasswordSecond, setRegisterPasswordSecond] =
+    useState('');
+
+  const submitRegister = () => {
+    if (
+      registerName.length > 2 &&
+      registerEmail.includes('@') &&
+      registerHome.length > 2 &&
+      registerPhone.length >= 8
+    ) {
+      if (registerPassword === registerPasswordSecond) {
+        alert('Register successful!');
+      } else {
+        alert('Passwords no match');
+      }
+    } else {
+      alert('Check the information provided');
+    }
+  };
+
   return (
     <div id="sign-up-div">
       <h3> REGISTER </h3>
       <body>
-        <form action="/sign-up-page">
-          Full Name: <input type="text"/> <br/>
-          Email Address: <input type="text"/> <br/>
-          Home Address: <input type="text"/> <br/>
-          Phone Number: <input type="text"/> <br/>
-          Password: <input type="text"/> <br/>
-          Confirm Password: <input type="text"/> <br/>
+        <form action="/sign-up-page" onSubmit={submitRegister}>
+          Full Name:{' '}
+          <input
+            value={registerName}
+            type="text"
+            onChange={(e) => setRegisterName(e.target.value)}
+          />{' '}
+          <br />
+          Email Address:{' '}
+          <input
+            value={registerEmail}
+            type="text"
+            onChange={(e) => setRegisterEmail(e.target.value)}
+          />{' '}
+          <br />
+          Home Address:{' '}
+          <input
+            value={registerHome}
+            type="text"
+            onChange={(e) => setRegisterHome(e.target.value)}
+          />{' '}
+          <br />
+          Phone Number:{' '}
+          <input
+            value={registerPhone}
+            type="number"
+            onChange={(e) => setRegisterPhone(e.target.value)}
+          />{' '}
+          <br />
+          Password:{' '}
+          <input
+            value={registerPassword}
+            type="password"
+            onChange={(e) => setRegisterPassword(e.target.value)}
+          />{' '}
+          <br />
+          Confirm Password:{' '}
+          <input
+            value={registerPasswordSecond}
+            type="password"
+            onChange={(e) =>
+              setRegisterPasswordSecond(e.target.value)
+            }
+          />{' '}
+          <br />
           <button type="Register"> Register </button>
         </form>
       </body>
