dec56e48c6e43b9b674b41becff20d14c4379aa2
Taylor <taylor.dobson@ttu.edu>
2022-11-30 18:49:01 -0600

    td

---
+++ src/components/review.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './login.css';
+import './general.css';
+
+function Review() {
+    
+      return (
+        <div id="purchase-div">
+          <h3> MOVIE REVIEW </h3>
+            <body>
+              <h4> Share your experience! </h4>
+              <form action="/purchase-page">
+                <textarea /><br/>
+                Rating: <input type="text" placeholder="1-5"/><br/>
+                <button type="Submit">Submit</button>
+              </form>
+            </body>
+        </div>
+      );
+    }
+
+export default Review;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Header from './components/header';
-import Purchase from './components/purchase';
-import Barcode from './components/barcode';
+import Review from './components/review';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Purchase />
-    <Barcode />
+    <Review />
   </React.StrictMode>
 );
