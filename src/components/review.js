fcdc019e7ce206638ebccc86029443a3bf0ccbc6
Miguel Cruz <Miggy2146@yahoo.com>
2022-12-03 18:13:11 -0600

    added to review

--- src/components/review.js
+++ src/components/review.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './login.css';
 import './general.css';
+import { useNavigate } from 'react-router-dom';
 
 function Review() {
+
+  const toCatalog = useNavigate();
+  const backToHome = () => {
+    alert('review has been made');
+    toCatalog('/catalog');
+  }
     
       return (
         <div id="purchase-div">
@@ -11,7 +18,7 @@ function Review() {
               <h4> Share your experience! </h4>
               <form action="/purchase-page">
                 <textarea /><br/>
-                <button type="Submit">Submit</button>
+                <button type="Submit" onClick={ backToHome }>Submit</button>
               </form>
             </body>
         </div>
